9330d24c37f87aefe95e592020316d0a961bd2b1
3DJ <71472458+ThreeDeeJay@users.noreply.github.com>
2024-09-07 14:28:13 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,9 +11,9 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         flatDir {
-            dirs '/home/runner/work/Simple-Gallery/Simple-Gallery/app/lib'
+            dirs '/home/runner/work/Simple-Gallery/Simple-Gallery/app/lib';
         }
-        maven { url "https://chromium.googlesource.com/external/github.com/googlevr/gvr-android-sdk/+/25a0c20415bd3854b76f3e0e55f73d36cdc076fd/libraries" }
+        maven(url = "https://chromium.googlesource.com/external/github.com/googlevr/gvr-android-sdk/+/25a0c20415bd3854b76f3e0e55f73d36cdc076fd/libraries")
         google()
         jcenter()
         mavenCentral()
